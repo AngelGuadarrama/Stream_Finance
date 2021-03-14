7539d70bac67e2c6ec3f0d141c7723d51d3dcaa3
AngelGuadarrama <61916656+AngelGuadarrama@users.noreply.github.com>
2021-03-14 01:08:39 -0600

    Create app_finance.py

---
+++ app_finance.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+tickerSymbol = 'AMZN'
+
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='1d', start='2015-12-31', end='2020-12-31')
+
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
