68798abd65ad09b99a3982cd75ac81db876082f8
AngelGuadarrama <angel0998@ciencias.unam.mx>
2021-03-21 21:18:17 -0600

    period

--- app_finance.py
+++ app_finance.py
@@ -1,17 +1,54 @@
-import yfinance as yf
 import streamlit as st
+import yfinance as yf
+import pandas as pd
+import cufflinks as cf
+import datetime
 
-st.write("""
-# Simple Stock Price App
-Shown are the stock closing price and volume of Tesla!
-""")
+# App title
+st.markdown('''
+# Choose the stock you want, the period, the start and finish date
+''')
+st.write('---')
 
-tickerSymbol = 'TSLA'
+# Sidebar
+st.sidebar.subheader('Parameters')
+start_date = st.sidebar.date_input("Start date", datetime.date(2019, 2, 28))
+end_date = st.sidebar.date_input("End date", datetime.date(2021, 2, 28))
 
-tickerData = yf.Ticker(tickerSymbol)
-tickerDf = tickerData.history(period='1d', start='2015-12-31', end='2020-12-31')
+# Retrieving tickers data
+ticker_list = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
+tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
 
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+ticker_periods= ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max']
+ticker_period = st.sidebar.selectbox('Stock period', ticker_periods) # Select ticker symbol
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+tickerData = yf.Ticker(tickerSymbol) # Get ticker data
+tickerDf = tickerData.history(period= ticker_period, start=start_date, end=end_date) #get the historical prices for this ticker
+
+# Ticker information
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(string_logo, unsafe_allow_html=True)
+
+string_name = tickerData.info['longName']
+st.header('**%s**' % string_name)
+
+string_name = tickerData.info['sector']
+st.write('**%s**' % string_name)
+
+string_name = tickerData.info['country']
+st.write('**%s**' % string_name)
+
+string_name = tickerData.info['website']
+st.write('**%s**' % string_name)
+
+string_summary = tickerData.info['longBusinessSummary']
+st.info(string_summary)
+
+st.header('**Ticker data**')
+st.write(tickerDf)
+
+st.header('**Bollinger Bands**')
+qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
+qf.add_bollinger_bands()
+fig = qf.iplot(asFigure=True)
+st.plotly_chart(fig)
